83aa75657594eb420365604dfb03fcc82d518fea
M Omar Faruque Sarker <writefaruq@gmail.com>
2009-06-19 21:15:59 +0100

    Fixed warnings from dmtxd.c

--- src/dmtxd.c
+++ src/dmtxd.c
@@ -26,8 +26,13 @@
 #include <signal.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
 
-#define RUNNING_DIR	"/tmp"
+#include "inotify.h"
+
+#define RUNNING_DIR	"/tmp/dmtxdatadir"
 #define LOCK_FILE	"dmtxd.lock"
 #define LOG_FILE	"dmtxd.log"
 
@@ -58,35 +63,101 @@ int sig;
 
 void daemonize()
 {
-int i,lfp;
-char str[10];
-	if(getppid()==1) return; /* already a daemon */
-	i=fork();
-	if (i<0) exit(1); /* fork error */
-	if (i>0) exit(0); /* parent exits */
+        int i,lfp,ret;
+        char str[10];
+
+	if(getppid()==1)
+                return; /* already a daemon */
+	ret=fork();
+	if (ret<0)
+                exit(EXIT_FAILURE); /* fork error */
+	if (ret>0)
+                exit(EXIT_SUCCESS); /* parent exits */
+
 	/* child (daemon) continues */
-	setsid(); /* obtain a new process group */
-	for (i=getdtablesize();i>=0;--i) close(i); /* close all descriptors */
-	i=open("/dev/null",O_RDWR); dup(i); dup(i); /* handle standart I/O */
-	umask(027); /* set newly created file permissions */
-	chdir(RUNNING_DIR); /* change running directory */
+	/* obtain a new process group */
+	setsid();
+
+	/* close all descriptors */
+	for (i=getdtablesize();i>=0;--i)
+                close(i);
+
+	/* handle standart I/O */
+	ret=open("/dev/null",O_RDWR);
+	ret = dup(i);
+	ret = dup(i);
+
+	if (ret<0)
+                exit(EXIT_FAILURE);
+	if (ret>0)
+                exit(EXIT_SUCCESS);
+
+
+	/* set newly created file permissions */
+	umask(027);
+
+	/* change running directory */
+	ret = chdir(RUNNING_DIR);
+	if (ret<0)
+                exit(EXIT_FAILURE);
+	if (ret>0)
+                exit(EXIT_SUCCESS);
+
+
 	lfp=open(LOCK_FILE,O_RDWR|O_CREAT,0640);
-	if (lfp<0) exit(1); /* can not open */
-	if (lockf(lfp,F_TLOCK,0)<0) exit(0); /* can not lock */
+
+	/* can not open */
+	if (lfp<0)
+                exit(EXIT_FAILURE);
+	 /* can not lock */
+	if (lockf(lfp,F_TLOCK,0)<0)
+                exit(EXIT_SUCCESS);
+
 	/* first instance continues */
 	sprintf(str,"%d\n",getpid());
-	write(lfp,str,strlen(str)); /* record pid to lockfile */
-	signal(SIGCHLD,SIG_IGN); /* ignore child */
-	signal(SIGTSTP,SIG_IGN); /* ignore tty signals */
+
+	/* record pid to lockfile */
+	ret = write(lfp,str,strlen(str));
+
+        if (ret<0)
+                exit(EXIT_FAILURE);
+	if (ret>0)
+                exit(EXIT_SUCCESS);
+
+
+	/* ignore child */
+	signal(SIGCHLD,SIG_IGN);
+
+	/* ignore tty signals */
+	signal(SIGTSTP,SIG_IGN);
 	signal(SIGTTOU,SIG_IGN);
 	signal(SIGTTIN,SIG_IGN);
-	signal(SIGHUP,signal_handler); /* catch hangup signal */
-	signal(SIGTERM,signal_handler); /* catch kill signal */
+
+        /* catch hangup signal */
+	signal(SIGHUP,signal_handler);
+
+	/* catch kill signal */
+	signal(SIGTERM,signal_handler);
 }
 
-main()
+int main(int argc, char *argv[])
 {
+	int err;
 	daemonize();
-	while(1) sleep(1); /* run */
+
+	while(1){
+                /* run inotify */
+                err = inotify_watcher();
+                if (err < 0) {
+                        printf("inotify: %s (%d)", strerror(errno), errno);
+                }
+                sleep(1);
+	}
+
+        printf("\nExiting\n");
+
+	exit(EXIT_SUCCESS);
+
+	return 0;
 }
 
